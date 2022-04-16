5c7b5cf524763500c0f740fc0107bab8ad41e600
Mali Matiso <s220651337@mandela.ac.za>
2022-04-17 00:23:41 +0200

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,7 +1,7 @@
 export const products = [
   {
     id: 'box-of-cookies',
-    name: 'Box',
+    name: 'Cookie Box',
     description: 'A delicious box of handmade cookies',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
@@ -9,10 +9,18 @@ export const products = [
   },
   {
     id: 'basket-of-cookies',
-    name: 'Basket',
+    name: 'Cookie Basket',
     description: 'A large basket of handmade cookies',
     unitName: 'basket',
     priceSol: 0.1,
     priceUsd: 10,
+  },
+  {
+    id: 'box-of-brownies',
+    name: 'Brownie Box',
+    description: 'A box of homemade brownies',
+    unitName: 'box',
+    priceSol: 0.02,
+    priceUsd: 2,
   }
 ]
